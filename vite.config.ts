11b0f60ef20c05c570870ea44c6e7c6836c8d7c8
sepehr zarghami <sepeehrz@gmail.com>
2024-05-06 18:10:12 +0330

    add hash in css modules

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,30 @@
-import {fileURLToPath, URL} from 'node:url';
-
 import {defineConfig} from 'vite';
 import vue from '@vitejs/plugin-vue';
 import svgLoader from 'vite-svg-loader';
+import {fileURLToPath, URL} from 'node:url';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  server: {
-    port: 9001
-  },
-
-  plugins: [vue(), svgLoader()],
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData:
-          '@import "./src/app/ui/assets/style/settings/_index.scss";'
+export default defineConfig(({mode}) => {
+  return {
+    server: {
+      port: 9001
+    },
+    plugins: [vue(), svgLoader()],
+    css: {
+      modules: {
+        generateScopedName: mode === 'production' ? '[hash:base64:8]' : ''
+      },
+      preprocessorOptions: {
+        scss: {
+          additionalData:
+            '@import "./src/app/ui/assets/style/settings/_index.scss";'
+        }
+      }
+    },
+    resolve: {
+      alias: {
+        '@modules': fileURLToPath(new URL('./src/modules', import.meta.url)),
+        '@app': fileURLToPath(new URL('./src/app', import.meta.url))
       }
     }
-  },
-  resolve: {
-    alias: {
-      '@modules': fileURLToPath(new URL('./src/modules', import.meta.url)),
-      '@app': fileURLToPath(new URL('./src/app', import.meta.url))
-    }
-  }
+  };
 });
